378e0e6b1d6874aeccd146ac46f076435709bc24
de.toolcreator <de.toolcreator@gmail.com>
2020-12-30 17:09:18 +0100

    Implement retrieving stats

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import { DockerStatsRecord } from './DockerStatsRecord';
+import { dockerContainers, dockerContainerStats } from 'dockerstats';
+
+let intervalMs = 1000;
+
+async function getStats() {
+  let stats : DockerStatsRecord[] = [];
+
+  const containers = await dockerContainers();
+  if (containers) {
+    const containerIds = containers.map(c => c.id);
+    const containerIdsStr = containerIds.reduce((p, c) => p + ', ' + c);
+    const containerNames = containers.map(c => c.name)
+    stats = (await dockerContainerStats(containerIdsStr)).map(s => {
+      return new DockerStatsRecord(
+        s.id,
+        containerNames[containerIds.findIndex(id => id == s.id)],
+        s.cpu_percent,
+        s.mem_usage,
+        s.mem_limit,
+        s.mem_percent,
+        s.netIO.rx,
+        s.netIO.wx,
+        s.blockIO.r,
+        s.blockIO.w
+      );
+    });
+  }
+  return stats;
+}
+
+const intervalId = setInterval(
+  async () => {
+    const stats = await getStats();
+  },
+  intervalMs
+);
