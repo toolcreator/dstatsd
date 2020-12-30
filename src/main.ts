8e3eabeaf998e5681621d0161c843ee0f1862691
de.toolcreator <de.toolcreator@gmail.com>
2020-12-30 21:18:04 +0100

    Add and expose metrics

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,87 @@
-import { DockerStatsRecord } from './DockerStatsRecord';
-import { dockerContainers, dockerContainerStats } from 'dockerstats';
+import { updateStats, getStats, DockerStatsRecord } from "./stats";
+import { collectDefaultMetrics, register, Gauge } from "prom-client";
+import express from "express";
 
+// TODO get from command line
 const intervalMs = 1000;
+const port = 9101;//8080;
 
-async function getStats() {
-  let stats : DockerStatsRecord[] = [];
-
-  const containers = await dockerContainers();
-
-  if (containers) {
-    const containerIds = containers.map(c => c.id);
-    const containerIdsStr = containerIds.reduce((p, c) => p + ', ' + c);
-    const containerNames = containers.map(c => c.name)
-
-    stats = (await dockerContainerStats(containerIdsStr)).map((s, i) => {
-      return new DockerStatsRecord(
-        s.id,
-        containerNames[i],
-        s.cpu_percent,
-        s.mem_usage,
-        s.mem_limit,
-        s.mem_percent,
-        s.netIO.rx,
-        s.netIO.wx,
-        s.blockIO.r,
-        s.blockIO.w
-      );
-    });
+const LABEL_NAMES = [
+  "container_id",
+  "container_name"
+];
+function defaultCollect<T extends string>(g: Gauge<T>, extractMetric: (s: DockerStatsRecord) => number) {
+  for (const stat of getStats()) {
+    const metric = extractMetric(stat);
+    g.labels(stat.id, stat.name).set(metric ? metric : 0);
   }
-
-  return stats;
 }
 
-async function update() {
-  const stats = await getStats();
-  console.log(stats);
-}
+new Gauge({
+  name: "dstats_cpu_percent",
+  help: "The percentage of the host’s CPU the container is using",
+  labelNames: LABEL_NAMES,
+  collect() { defaultCollect(this, s => s.cpuPercent); }
+});
+new Gauge({
+  name: "dstats_memory_usage_bytes",
+  help: "The total amount of memory the container is using",
+  labelNames: LABEL_NAMES,
+  collect() { defaultCollect(this, s => s.memUsage); }
+});
+new Gauge({
+  name: "dstats_memory_limit_bytes",
+  help: "The total amount of memory the container is allowed to use",
+  labelNames: LABEL_NAMES,
+  collect() { defaultCollect(this, s => s.memLimit); }
+});
+new Gauge({
+  name: "dstats_memory_percent",
+  help: "The percentage of the host’s memory the container is using",
+  labelNames: LABEL_NAMES,
+  collect() { defaultCollect(this, s => s.memPercent); }
+});
+new Gauge({
+  name: "dstats_network_received_bytes",
+  help: "The amount of data the container has received over its network interface",
+  labelNames: LABEL_NAMES,
+  collect() { defaultCollect(this, s => s.netInp); }
+});
+new Gauge({
+  name: "dstats_network_transmitted_bytes",
+  help: "The amount of data the container has transmitted over its network interface",
+  labelNames: LABEL_NAMES,
+  collect() { defaultCollect(this, s => s.netOut); }
+});
+new Gauge({
+  name: "dstats_block_read_bytes",
+  help: "The amount of data the container has read from block devices on the host",
+  labelNames: LABEL_NAMES,
+  collect() { defaultCollect(this, s => s.blockIn); }
+});
+new Gauge({
+  name: "dstats_block_written_bytes",
+  help: "The amount of data the container has written to block devices on the host",
+  labelNames: LABEL_NAMES,
+  collect() { defaultCollect(this, s => s.blockOut); }
+});
 
-setInterval(update, intervalMs);
+const daemon = express();
+daemon.get("/metrics", async (req, res) => {
+  try {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+  } catch (err) {
+    res.status(500).end(err.toString());
+  }
+});
+
+try {
+  collectDefaultMetrics();
+  setInterval(updateStats, intervalMs);
+  daemon.listen(port)
+  console.log(`Listening on port ${port}.`);
+  console.log("Metrics are exposed at /metrics endpoint.");
+} catch (err) {
+  console.log(err);
+}
---
+++ src/stats.ts
@@ -0,0 +1,52 @@
+import { dockerContainers, dockerContainerStats } from 'dockerstats';
+
+let dstats: DockerStatsRecord[] = [];
+
+export class DockerStatsRecord {
+  public constructor(
+    public id: string,
+    public name: string,
+    public cpuPercent: number,
+    public memUsage: number,
+    public memLimit: number,
+    public memPercent: number,
+    public netInp: number,
+    public netOut: number,
+    public blockIn: number,
+    public blockOut: number,
+    // public pids: number
+  ) { /* empty */ }
+}
+
+export async function updateStats(): Promise<void> {
+  try {
+    const containers = await dockerContainers();
+
+    if (containers) {
+      const containerIdsStr = containers.map(c => c.id).reduce((p, c) => p + ', ' + c);
+      const containerNames = containers.map(c => c.name)
+
+      dstats = (await dockerContainerStats(containerIdsStr)).map((s, i) => {
+        return new DockerStatsRecord(
+          s.id,
+          containerNames[i],
+          s.cpu_percent,
+          s.mem_usage,
+          s.mem_limit,
+          s.mem_percent,
+          s.netIO.rx,
+          s.netIO.wx,
+          s.blockIO.r,
+          s.blockIO.w
+        );
+      });
+      //console.log(dstats);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export function getStats(): DockerStatsRecord[] {
+  return dstats;
+}
